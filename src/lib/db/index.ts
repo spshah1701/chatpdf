499d5c38f9dcfc4604f71f3d4f26dc924f4e3dfe
spshah1701 <sshah210@asu.edu>
2024-03-21 12:51:34 -0700

    Resolved Drizzle issue

--- src/lib/db/index.ts
+++ src/lib/db/index.ts
@@ -1,5 +1,9 @@
 import { neon } from "@neondatabase/serverless";
 import { drizzle } from 'drizzle-orm/neon-http';
+import type { NeonQueryFunction } from "@neondatabase/serverless";
+
+
+
 
 // neonConfig.fetchConnectionCache = true;
 
@@ -7,7 +11,7 @@ if (!process.env.DATABASE_URL) {
   throw new Error("Database URL not found!");
 }
 
-const sql = neon(process.env.DATABASE_URL!);
+const sql: NeonQueryFunction<boolean, boolean> = neon(process.env.DATABASE_URL!);
 // console.log(sql)
 
 export const db = drizzle(sql);
